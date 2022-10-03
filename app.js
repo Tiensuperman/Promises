229b08373e8cb683b6ac1629d91385bbcd465bcf
Tiensuperman <ddtien@outlook.com>
2022-10-04 04:44:55 +0700

    Promises

---
+++ app.js
@@ -0,0 +1,30 @@
+//console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
+//const emailRef = document.querySelector(".email");
+
+//1. Then
+//fetch("https://jsonplaceholder.typicode.com/users/1")
+  //.then((response) => {   
+   //return response.json();
+  //})
+  //.then((data) => {
+    //console.log(data);
+    //emailRef.innerHTML = data.email
+  //});
+
+//2. Async/Await
+//async function main() {
+  //const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+  //const data = await response.json()
+  //console.log(data)
+  //emailRef.innerHTML = data.email
+//}
+
+//main()
+
+function getSubscriptionStatus() {
+  return new Promise((resolve, reject) => {
+    resolve("VIP")
+  })
+}
+
+console.log(getSubscriptionStatus())
